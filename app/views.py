938e62948809219e09f83e72a56f1168d7e78026
jthang <jensthang@gmail.com>
2014-12-20 15:56:39 +0800

    added bootstrap

--- app/views.py
+++ app/views.py
@@ -4,18 +4,6 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Jens'}
-    posts = [
-        {
-        'author': {'nickname': 'Wing'},
-        'body': 'Beautiful day in Matrix today!'
-        },
-        {
-        'author': {'nickname': 'Jens'},
-        'body': 'Yes, I agree. I love data!'
-        }
-    ]
+    # user = {'nickname': 'Jens'}
     return render_template('index.html',
-                            title='Home',
-                            user=user,
-                            posts=posts)
+                            title='Project Movie')
