db2c15bb0155b69ab3867d4a7d0ba5b961adbb51
jthang <jensthang@gmail.com>
2014-12-21 16:31:44 +0800

    update

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
-from flask import render_template
+from flask import Flask, jsonify, render_template, request
 from app import app
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
 
 @app.route('/')
 @app.route('/index')
@@ -7,3 +13,4 @@ def index():
     # user = {'nickname': 'Jens'}
     return render_template('index.html',
                             title='Project Movie')
+
